8b9f15650ab96d55a02309dbf5039a23344bb32e
daikugimiya0715 <daikugimiya0715@gmail.com>
2023-11-14 21:18:03 +0900

    Squashed commit of the following:
    commit c412bb5b4993a618bd110c8553b8586355eea440
    Author: daikugimiya0715 <daikugimiya0715@gmail.com>
    Date:   Mon Nov 13 23:11:01 2023 +0900
    
        😁 first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+  <h1 class="text-3xl mb-4 text-purple-500">
+        How to Install Tailwind Daisyui In Vue 3
+      </h1>
+      <button class="btn">Button</button>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-secondary">Button</button>
+      <button class="btn btn-accent">Button</button>
+      <button class="btn btn-ghost">Button</button>
+      <button class="btn btn-link">Button</button>
+  </div>
+</template>
